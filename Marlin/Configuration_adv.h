3a1cf29b895ebb75977216ce5fdbf19091530457
calguymike <mikedavidhall@outlook.com>
2021-01-30 13:13:07 +0000

    current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2344,7 +2344,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      715
+    #define E0_CURRENT      780
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
